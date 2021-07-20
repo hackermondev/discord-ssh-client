fd2735217b120a22abe73576951123dd934bcf26
Daniel <2026oyeenid@share.wilsonsd.org>
2021-07-20 11:11:41 +0200

    fix tests?

---
+++ scripts/actions/changeconfig.sh
@@ -0,0 +1,2 @@
+mv $GITHUB_WORKSPACE/src/config/config/botConfig.example.json $GITHUB_WORKSPACE/src/config/config/botConfig.json
+mv $GITHUB_WORKSPACE/src/config/config/database.example.json $GITHUB_WORKSPACE/src/config/config/database.json
